58aca97dd42e7fd9d4f70db5083acf4a54bb8bd3
Vansh Kapoor <vkapoor0112@gmail.com>
2024-02-29 00:05:50 +0530

    Removed the wrapper

--- src/HeroSection.tsx
+++ src/HeroSection.tsx
@@ -85,115 +85,113 @@ function HeroSection() {
   }, [isOffset]);
 
   return (
-    <>
-      <Box sx={{ position: "relative", zIndex: 1 }}>
+    <Box sx={{ position: "relative", zIndex: 1 }}>
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          zIndex: 11,
+        }}
+      >
+        <Navbar />
+      </Box>
+      <Box
+        sx={{
+          mb: "100%",
+          pb: "40%",
+          top: 0,
+          left: 0,
+          right: 0,
+          position: "relative",
+        }}
+      >
         <Box
           sx={{
+            width: "100%",
+            height: "56.25vw",
             position: "absolute",
-            top: 0,
-            zIndex: 11,
           }}
         >
-          <Navbar />
-        </Box>
-        <Box
-          sx={{
-            mb: "100%",
-            pb: "40%",
-            top: 0,
-            left: 0,
-            right: 0,
-            position: "relative",
-          }}
-        >
+          {/* Video Player */}
           <Box
             sx={{
-              width: "100%",
-              height: "56.25vw",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
               position: "absolute",
             }}
           >
-            {/* Video Player */}
-            <Box
+            <Suspense fallback={<CircularProgress />}>
+              <LazyVideoJsPlayer
+                options={videoJsOptions}
+                onReady={handlePlayerReady}
+              />
+            </Suspense>
+          </Box>
+
+          {/* Video Metadata & Controller Buttons */}
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <Stack
+              spacing={4}
               sx={{
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
+                bottom: "35%",
                 position: "absolute",
+                left: { xs: "4%", md: "60px" },
+                top: 0,
+                width: "36%",
+                zIndex: 10,
+                justifyContent: "flex-end",
               }}
             >
-              <Suspense fallback={<CircularProgress />}>
-                <LazyVideoJsPlayer
-                  options={videoJsOptions}
-                  onReady={handlePlayerReady}
-                />
-              </Suspense>
-            </Box>
+              <MaxLineTypography variant="h2" maxLine={1} color="#fff">
+                React great again?
+              </MaxLineTypography>
+              <MaxLineTypography variant="h5" maxLine={3} color="#fff">
+                Let's take a first look at React 19 and a variety of changes
+                will improve the developer experience. We compare React's
+                changes to other JavaScript frameworks like Svelte, Vue, and
+                Angular.
+              </MaxLineTypography>
+              <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+                <PlayButton size="large" />
+                <MoreInfoButton size="large" />
+              </Stack>
+            </Stack>
 
-            {/* Video Metadata & Controller Buttons */}
-            <Box
+            <Stack
+              direction="row"
+              spacing={2}
               sx={{
+                alignItems: "center",
                 position: "absolute",
-                top: 0,
-                left: 0,
                 right: 0,
-                bottom: 0,
-                width: "100%",
-                height: "100%",
+                bottom: "35%",
               }}
             >
-              <Stack
-                spacing={4}
-                sx={{
-                  bottom: "35%",
-                  position: "absolute",
-                  left: { xs: "4%", md: "60px" },
-                  top: 0,
-                  width: "36%",
-                  zIndex: 10,
-                  justifyContent: "flex-end",
-                }}
+              <NetflixIconButton
+                size="large"
+                onClick={() => handleMute(muted)}
+                sx={{ zIndex: 2 }}
               >
-                <MaxLineTypography variant="h2" maxLine={1} color="#fff">
-                  React great again?
-                </MaxLineTypography>
-                <MaxLineTypography variant="h5" maxLine={3} color="#fff">
-                  Let's take a first look at React 19 and a variety of changes
-                  will improve the developer experience. We compare React's
-                  changes to other JavaScript frameworks like Svelte, Vue, and
-                  Angular.
-                </MaxLineTypography>
-                <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
-                  <PlayButton size="large" />
-                  <MoreInfoButton size="large" />
-                </Stack>
-              </Stack>
-
-              <Stack
-                direction="row"
-                spacing={2}
-                sx={{
-                  alignItems: "center",
-                  position: "absolute",
-                  right: 0,
-                  bottom: "35%",
-                }}
-              >
-                <NetflixIconButton
-                  size="large"
-                  onClick={() => handleMute(muted)}
-                  sx={{ zIndex: 2 }}
-                >
-                  {!muted ? <VolumeUpIcon /> : <VolumeOffIcon />}
-                </NetflixIconButton>
-                <MaturityRate>{`18+`}</MaturityRate>
-              </Stack>
-            </Box>
+                {!muted ? <VolumeUpIcon /> : <VolumeOffIcon />}
+              </NetflixIconButton>
+              <MaturityRate>{`18+`}</MaturityRate>
+            </Stack>
           </Box>
         </Box>
       </Box>
-    </>
+    </Box>
   );
 }
 
